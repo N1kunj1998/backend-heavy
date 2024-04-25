7cbe070a10529ed1f216f56d63472f0301b8e3d9
N1kunj1998 <njkhakhkhar@gmail.com>
2024-04-25 21:07:30 +0530

    completed learning about making modules and exporting them

--- feature.js
+++ feature.js
@@ -1,3 +1,15 @@
 const name = "Random";
+const name1 = "name1";
+const name2 = "name2";
 
-module.exports = name;
+// module.exports = name;
+
+export default name; // In case of deault export you can use any different name while importing 
+export { name1, name2 }; //In case of only export you cannot use any different name
+
+export const name3 = "name3";
+
+// exporting functions
+export const generateLovePercent = function() {
+    return `${Math.floor(Math.random()*100)}%`;
+}
--- index.js
+++ index.js
@@ -1,11 +1,17 @@
-const http = require("http");
-const name = require("./feature");
+// const http = require("http");
+import http from "http";
+// const name = require("./feature");
+import myName from "./feature.js"
+import { name2, name1 } from "./feature.js";
+import * as myObj from "./feature.js"; // now everything from feature.js file will be accessed using this object.
 
-console.log(name);
+console.log(myName);
+console.log(myObj.name3);
+console.log(myObj.generateLovePercent())
 
 const server = http.createServer((req, res) => {
     if(req.url === "/about") {
-        res.end("<h1>about page</h1>");
+        res.end(`<h1>Love is ${myObj.generateLovePercent()}</h1>`);
     } else if(req.url === "/") {
         res.end("<h1>Home Page</h1>");
     } else if(req.url === "/contact") {
