4e8d4b292a65f7c65d897e4555aa40838aba2563
N1kunj1998 <njkhakhkhar@gmail.com>
2024-04-28 01:20:44 +0530

    added authentication part for basic app from next commit will start main project and will clear files

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 import express from "express";
 import path from "path";
 import mongoose from "mongoose";
+import cookieParser from "cookie-parser";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
 
 mongoose.connect("mongodb://localhost:27017", {
     dbName: "backend",
@@ -9,12 +12,13 @@ mongoose.connect("mongodb://localhost:27017", {
 .catch((e) => console.error(e));
 
 // Defining Schema
-const messageSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema({
     name: String,
     email: String,
+    password: String,
 });
 
-const Message = mongoose.model("Message", messageSchema);
+const User = mongoose.model("User", userSchema);
 
 const app = express();
 
@@ -24,29 +28,90 @@ const users = [];
 // while linking some files like css or js if the file is not found in current location then 
 // it will also be searched in this public folder because we have made it static
 app.use(express.static(path.join(path.resolve(),"./public")));
-
 app.use(express.urlencoded({extended:true}));
+app.use(cookieParser());
 
 // setting up View Engine
 app.set("view engine", "ejs")
 
-app.get("/", (req, res) => {
-    res.render("index.ejs", {name: "nikunj"});
+const isAuthenticated = async (req, res, next) => {
+    const {token} = req.cookies;
+    if(token) {
+        const decoded = jwt.verify(token, "adsflakjdfgal");
+        // console.log(decoded);
+        req.user = await User.findById(decoded._id);
+        next();
+    } else {
+        res.redirect("/login");
+    }
+}
+
+app.get("/", isAuthenticated , (req, res) => {
+    // console.log(req.user);
+    res.render("logout", {name: req.user.name});
 })
 
-app.get("/success", (req, res) => {
-    res.render("success.ejs");
+app.get("/register", (req, res) => {
+    res.render("register");
 })
 
-app.get("/users", (req, res) => {
-    res.json({users});
+app.get("/logout", (req, res) => {
+    res.cookie("token", null, {
+        httpOnly: true,
+        expires: new Date(Date.now()),
+    });
+    res.redirect("/");
 })
 
-app.post("/contact", async (req, res) => {
-    const {name, email} = req.body;
-    await Message.create({name, email});
-    res.redirect("/success");
+app.get("/login", (req, res) => {
+    res.render("login");
 })
+
+app.post("/login", async (req, res) => {
+    const { email, password } = req.body;
+
+    let user = await User.findOne({email});
+    if(!user) return res.redirect("/register");
+
+    const isMatch = user.password === password;
+
+    if(!isMatch) return res.render("login", {email, message: "Incorrect Password"});
+
+    const token = jwt.sign({_id: user._id}, "adsflakjdfgal");
+
+    res.cookie("token", token, {
+        httpOnly: true, // it is good to keep this true
+        expires: new Date(Date.now() + 60 * 1000),
+    });
+    res.redirect("/");
+})
+
+app.post("/register", async (req, res) => {
+    const {name, email, password} = req.body;
+
+    let user = await User.findOne({email});
+    console.log(!user);
+    if(user != null) {
+        return res.redirect("/login");
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    user = await User.create({
+        name,
+        email,
+        password: hashedPassword
+    });
+
+    const token = jwt.sign({_id: user._id}, "adsflakjdfgal");
+
+    res.cookie("token", token, {
+        httpOnly: true, // it is good to keep this true
+        expires: new Date(Date.now() + 60 * 1000),
+    });
+    res.redirect("/");
+})
+
 
 app.listen(5001, () => {
     console.log(`server listening at port 5001`);
