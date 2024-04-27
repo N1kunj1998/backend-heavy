83390be04211ce4e1f28ecc83ddc55052328e6ce
N1kunj1998 <njkhakhkhar@gmail.com>
2024-04-27 12:03:59 +0530

    all basic setup done and seen next will make small project with this.

--- index.js
+++ index.js
@@ -1,26 +1,21 @@
 import express from "express";
 import path from "path";
 
-// As per industry standards instead of name of the server server we keep it as app
 const app = express();
 
-app.get("/", (req, res) => {
-    // res.statusCode(404);
-    
-    // res.send("hi");
-    
-    // res.json({
-    //     success: true,
-    //     products: []
-    // })
-    
-    // res.status(400).send("meri marzi");
+// express.static is a middleware so it cannot be used directly we need to use app.use()
+// while linking some files like css or js if the file is not found in current location then 
+// it will also be searched in this public folder because we have made it static
+app.use(express.static(path.join(path.resolve(),"./public")));
 
-    // Sending html file as response
-    // console.log(path.resolve())
-    const pathLocation = path.resolve();
-    // res.sendFile(path.join(pathLocation, "./index.html"));
-    res.render()
+// setting up View Engine
+app.set("view engine", "ejs")
+
+app.get("/", (req, res) => {
+    res.render("index.ejs", {name: "nikunj"});
+
+    // since we have set static folder we can directly send content from this
+    // res.sendFile("index");
 })
 
 app.listen(5001, () => {
